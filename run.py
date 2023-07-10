14481e50cd5ea6278c48bf59feb2198bfd7ebcef
Urwa-Iqbal <131991528+Urwa-Iqbal@users.noreply.github.com>
2023-07-10 22:23:30 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,29 @@
+# Dont make changes in this file
+import unittest
+import sys
+from io import StringIO
+
+from Test import *
+
+class TestOutput(unittest.TestCase):
+    def test_output(self):
+        expected_output ="""  * 
+                              * *
+                              * * *
+                              * * * *
+                              * * * * * """
+
+        # Redirect sys.stdout to capture the output
+        sys.stdout = captured_output = StringIO()
+
+        # Call the student's code
+        print_name()
+
+        # Restore sys.stdout
+        sys.stdout = sys._stdout_
+
+        # Compare the expected output with the captured output
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
+if _name_ == '_main_':
+    unittest.main()
